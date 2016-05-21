35c82fd6f321d05436c3281989f415a00512dc16
Ilya Bohaslauchyk <3751950@gmail.com>
2016-05-21 23:09:58 +0300

    Second lesson
    1. Added config
    2. Added logger

---
+++ config/index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Phaeton on 21.05.2016.
+ */
+var nconf = require("nconf");
+var path = require("path");
+
+nconf.argv()
+     .env()
+     .file({file: path.join(__dirname, "config.json")});
+
+module.exports = nconf;
